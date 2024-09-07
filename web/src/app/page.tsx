1910b8444bdce8a0e41b147028abeae7459732a4
temitopeohassan <temitope.hassan.2@gmail.com>
2024-09-07 19:34:11 +0100

    corrections

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -18,7 +18,7 @@ const frameMetadata = getFrameMetadata({
     },
   ],
   image: {
-    src: `${NEXT_PUBLIC_URL}/park-3.png`,
+    src: `${NEXT_PUBLIC_URL}/image.jpg`,
     aspectRatio: '1:1',
   },
   input: {
